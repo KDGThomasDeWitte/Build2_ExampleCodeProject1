edf6e79b96196220e5606e8a7a5e268419c012b6
hans.vangompel@hbplus.be <c0nGtdSORt1ITonxIfc6>
2023-02-11 18:24:12 +0100

    Initiële Bluetooth BLE voorbeelden les 2

---
+++ les2_bluetooth/les2_1_connecteer_via_ble/les2_1_connecteer_via_ble.ino
@@ -0,0 +1,30 @@
+#include <BLEDevice.h>
+#include <BLEServer.h>
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      Serial.println("Device connected...");
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      Serial.println("Device disconnected...");
+    }
+};
+
+void setup() {
+  Serial.begin(9600);
+
+  // Create the BLE Device
+  BLEDevice::init("ESP32");
+
+  // Create the BLE Server
+  BLEServer* pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Start advertising
+  pServer->getAdvertising()->start();
+  Serial.println("Waiting for a client connection...");
+}
+
+void loop() {
+}
---
+++ les2_bluetooth/les2_2_verstuur_gegevens_via_notify/les2_2_verstuur_gegevens_via_notify.ino
@@ -0,0 +1,76 @@
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+BLECharacteristic* pCharacteristic = NULL;
+bool deviceConnected = false;
+int txValue = 0;
+
+#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+//#define BLE_UUID_HEART_RATE_SERVICE               "180D"
+//#define BLE_UUID_HEART_RATE_MEASURMENT            "2A37"
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+      Serial.println("Device connected...");
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+      Serial.println("Device disconnected...");
+    }
+};
+
+void setup() {
+  Serial.begin(9600);
+
+  // Create the BLE Device
+  BLEDevice::init("ESP32");
+
+  // Create the BLE Server
+  BLEServer* pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID_TX,
+                      BLECharacteristic::PROPERTY_NOTIFY
+                    );
+
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  // Create a BLE Descriptor
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  pServer->getAdvertising()->start();
+  Serial.println("Waiting for a client connection to notify...");
+}
+
+void loop() {
+  // notify changed value
+  if (deviceConnected) {
+    txValue = random(-10, 20);
+
+    // Conversion of txValue
+    char txString[8];
+    dtostrf(txValue, 1, 2, txString);
+
+    // Setting the value to the characteristic
+    pCharacteristic->setValue(txString);
+
+    // Notifying the connected client
+    pCharacteristic->notify();
+    Serial.println("Sent value: " + String(txString));
+
+    delay(500);
+  }
+}
